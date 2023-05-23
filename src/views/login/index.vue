37c66b98ab7c7d0084c1445ee15e5b405980d362
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-23 23:24:51 +0800

    feat: 实现登录页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,69 @@
 <template>
-  <div>
-    <h1>登录</h1>
+  <div class="login_container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>欢迎来到硅谷甄选</h2>
+          <el-form-item>
+            <el-input :prefix-icon="User" v-model="loginForm.username" />
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="password"
+              :prefix-icon="Lock"
+              v-model="loginForm.password"
+              show-password
+            />
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login_btn" type="primary">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { User, Lock } from '@element-plus/icons-vue'
+import { reactive } from 'vue'
+//收集账号与密码数据
+let loginForm = reactive({
+  username: '',
+  password: '',
+})
+</script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.login_container {
+  width: 100%;
+  height: 100vh;
+  background: url('@/assets/images/background.jpg') no-repeat;
+  background-size: cover;
+  .login_form {
+    position: relative;
+    width: 80%;
+    top: 30vh;
+    background: url('@/assets/images/login_form.png');
+    background-size: cover;
+    padding: 40px;
+
+    h1 {
+      color: #fff;
+      font-size: 40px;
+    }
+
+    h2 {
+      font-size: 20px;
+      color: #fff;
+      margin: 20px 0;
+    }
+
+    .login_btn {
+      width: 100%;
+    }
+  }
+}
+</style>

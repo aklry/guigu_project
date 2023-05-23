d3d33e7d1f67affb32ee10a347f76b362264f6d1
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-23 18:34:34 +0800

    chore: SVG图标的配置

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import 'element-plus/dist/index.css'
 //@ts-expect-error忽略当前文件ts类型的检测否则有红色提示(打包会失败)
 //配置element-plus国际化
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'virtual:svg-icons-register'
 const app = createApp(App)
 //将应用挂载到挂载点
 app.use(ElementPlus, {

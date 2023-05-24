f3673252115f1e9ae551ac468f37c4e12ca9f42d
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-24 18:30:52 +0800

    feat: 实现首页的布局

--- src/components/Layout/index.vue
+++ src/components/Layout/index.vue
@@ -1,44 +1,42 @@
 <template>
-    <div class="layout_container">
-        <!-- 左侧菜单 -->
-        <div class="layout_slider"></div>
-        <!-- 顶部导航 -->
-        <div class="layout_tabbar"></div>
-        <!-- 内容展示区域 -->
-        <div class="layout_main"></div>
-    </div>
+  <div class="layout_container">
+    <!-- 左侧菜单 -->
+    <div class="layout_slider"></div>
+    <!-- 顶部导航 -->
+    <div class="layout_tabbar"></div>
+    <!-- 内容展示区域 -->
+    <div class="layout_main"></div>
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss" scoped>
 @import '@/styles/variable.scss';
 .layout_container {
-    width: 100%;
+  width: 100%;
+  height: 100vh;
+  .layout_slider {
+    width: $sliderWidth;
     height: 100vh;
-    .layout_slider {
-        width: $sliderWidth;
-        height: 100vh;
-        background-color: $sliderBackgroundColor;
-    }
-    .layout_tabbar {
-        position: fixed;
-        width: calc(100% - $sliderWidth);
-        height: $tabbarHeight;
-        top: 0;
-        left: $sliderWidth;
-    }
+    background-color: $sliderBackgroundColor;
+  }
+  .layout_tabbar {
+    position: fixed;
+    width: calc(100% - $sliderWidth);
+    height: $tabbarHeight;
+    top: 0;
+    left: $sliderWidth;
+  }
 
-    .layout_main {
-        position: absolute;
-        width: calc(100% - $sliderWidth);
-        height: calc(100vh - $tabbarHeight);
-        top: $tabbarHeight;
-        left: $sliderWidth;
-        padding: 20px;
-        overflow: auto;
-    }
+  .layout_main {
+    position: absolute;
+    width: calc(100% - $sliderWidth);
+    height: calc(100vh - $tabbarHeight);
+    top: $tabbarHeight;
+    left: $sliderWidth;
+    padding: 20px;
+    overflow: auto;
+  }
 }
 </style>

62c7f4136419e032baca011bd24d43e5e9956e68
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-24 16:28:56 +0800

    feat: 使用pinia仓库封装用户登录逻辑的方法

---
+++ src/store/modules/user.ts
@@ -0,0 +1,34 @@
+import { defineStore } from 'pinia'
+//引入接口
+import { reqLogin } from '@/api/user'
+//引入数据类型
+import type { loginForm, loginResponseData } from '@/api/user/type'
+import type { UserState } from './types/type'
+import { SET_TOKEN, GET_TOKEN } from '@/utils/token'
+export const userStore = defineStore('User', {
+  state: (): UserState => {
+    return {
+      token: GET_TOKEN(), //用户唯一标识
+    }
+  },
+  actions: {
+    //用户登录逻辑
+    async userLogin(data: loginForm) {
+      //登录请求
+      let result: loginResponseData = await reqLogin(data)
+      //登录请求:成功200->token
+      //登录请求:失败201->错误信息
+      if (result.code === 200) {
+        //存储token
+        this.token = result.data.token as string
+        //本地持久化token
+        SET_TOKEN(result.data.token as string)
+        //保证async函数返回成功的promise
+        return 'ok'
+      } else {
+        return Promise.reject(new Error(result.data.message))
+      }
+    },
+  },
+  getters: {},
+})

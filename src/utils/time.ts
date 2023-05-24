bada59c2efbba116cf3c2e4150718eba7537ffea
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-24 17:43:22 +0800

    feat: 封装根据时间返回上午|下午|晚上结果的方法

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,15 +1,15 @@
 //获取一个结果: 早上|下午|晚上
 export const getTime = (): string => {
-    let message = ''
-    let hours: number = new Date().getHours()
-    if (hours <= 9) {
-        message = '早上'
-    } else if (hours <= 12) {
-        message = '上午'
-    } else if (hours <= 18) {
-        message = '下午'
-    } else {
-        message = '晚上'
-    }
-    return message
+  let message = ''
+  let hours: number = new Date().getHours()
+  if (hours <= 9) {
+    message = '早上'
+  } else if (hours <= 12) {
+    message = '上午'
+  } else if (hours <= 18) {
+    message = '下午'
+  } else {
+    message = '晚上'
+  }
+  return message
 }

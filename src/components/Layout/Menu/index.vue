ab3dd5dd6aa4ea986190592128f0cc067b168b37
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-25 18:33:22 +0800

    feat: 编写组件动态生成左侧菜单栏

---
+++ src/components/Layout/Menu/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <template v-for="item in menuList" :key="item.path">
+    <template v-if="!item.children">
+      <!-- 没有子路由 -->
+      <el-menu-item v-if="!item.meta.hidden" :index="item.path">
+        <template #title>
+          <el-icon>
+            <component :is="item.meta.icon"></component>
+          </el-icon>
+          <span>{{ item.meta.title }}</span>
+        </template>
+      </el-menu-item>
+    </template>
+    <template v-if="item.children && item.children.length === 1">
+      <!-- 只有一个子路由 -->
+      <el-menu-item
+        v-if="!item.children[0].meta.hidden"
+        :index="item.children[0].path"
+      >
+        <template #title>
+          <el-icon>
+            <component :is="item.children[0].meta.icon"></component>
+          </el-icon>
+          <span>{{ item.children[0].meta.title }}</span>
+        </template>
+      </el-menu-item>
+    </template>
+    <template v-if="item.children && item.children.length > 1">
+      <!-- 有多个子路由 -->
+      <el-sub-menu v-if="!item.meta.hidden" :index="item.path">
+        <template #title>
+          <el-icon>
+            <component :is="item.meta.icon"></component>
+          </el-icon>
+          <span>{{ item.meta.title }}</span>
+        </template>
+        <Menu :menuList="item.children" />
+      </el-sub-menu>
+    </template>
+  </template>
+</template>
+
+<script setup lang="ts">
+defineProps(['menuList'])
+</script>
+<script lang="ts">
+export default {
+  name: 'Menu',
+}
+</script>
+<style scoped></style>

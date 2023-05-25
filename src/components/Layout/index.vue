3ba8ccab52760e17c4371e8b91f484a115e1b5c3
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-25 18:37:40 +0800

    feat: 实现layout布局

--- src/components/Layout/index.vue
+++ src/components/Layout/index.vue
@@ -3,29 +3,56 @@
     <!-- 左侧菜单 -->
     <div class="layout_slider">
       <Logo />
+      <!-- 展示菜单 -->
+      <!-- 滚动组件 -->
+      <el-scrollbar class="scrollbar">
+        <!-- 菜单组件 -->
+        <el-menu background-color="#001529" text-color="#fff" router>
+          <!-- 根据路由动态生成菜单 -->
+          <Menu :menuList="useUserStore.menuRoutes" />
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!-- 顶部导航 -->
     <div class="layout_tabbar"></div>
     <!-- 内容展示区域 -->
-    <div class="layout_main"></div>
+    <div class="layout_main">
+      <Main />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 //引入左侧菜单的logo
 import Logo from './Logo/index.vue'
+import Menu from './Menu/index.vue'
+//引入右侧内容展示区域组件
+import Main from './Main/index.vue'
+//获取用户相关仓库
+import { userStore } from '@/store/modules/user'
+const useUserStore = userStore()
 </script>
 
 <style lang="scss" scoped>
 @import '@/styles/variable.scss';
+
 .layout_container {
   width: 100%;
   height: 100vh;
+
   .layout_slider {
     width: $sliderWidth;
     height: 100vh;
     background-color: $sliderBackgroundColor;
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $sliderLogoHeight);
+      .el-menu {
+        border-right: none;
+      }
+    }
   }
+
   .layout_tabbar {
     position: fixed;
     width: calc(100% - $sliderWidth);

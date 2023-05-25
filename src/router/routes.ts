4ce23df9f133f84d70f8f8fd3dbd7598aabd5c72
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-25 18:34:13 +0800

    refactor: 编写左侧菜单栏的相关路由

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,20 +3,146 @@ export const routes = [
     path: '/login',
     name: 'Login',
     component: () => import('@/views/login/index.vue'),
+    meta: {
+      title: '登录',
+      hidden: true, //代表该路由需要隐藏
+      icon: 'Promotion',
+    },
   },
   {
     path: '/',
-    name: 'Home',
+    name: 'Layout',
     component: () => import('@/components/Layout/index.vue'),
+    meta: {
+      title: 'layout',
+      icon: 'Avatar',
+    },
+    redirect: '/home',
+    children: [
+      {
+        path: '/home',
+        component: () => import('@/views/home/index.vue'),
+        name: 'Home',
+        meta: {
+          title: '首页',
+
+          icon: 'HomeFilled',
+        },
+      },
+    ],
   },
   {
     path: '/404',
     name: 'NotFound',
     component: () => import('@/views/404/index.vue'),
+    meta: {
+      title: '404',
+      hidden: true,
+      icon: 'DocumentDelete',
+    },
   },
   {
     path: '/:pathMatch(.*)*',
     redirect: '/404',
     name: 'Any',
+    meta: {
+      title: '任意',
+      hidden: true,
+      icon: 'DataLine',
+    },
+  },
+  {
+    path: '/screen',
+    name: 'Screen',
+    meta: {
+      title: '数据大屏',
+      icon: 'Platform',
+    },
+    component: () => import('@/views/screen/index.vue'),
+  },
+  {
+    path: '/acl',
+    name: 'Acl',
+    component: () => import('@/components/Layout/index.vue'),
+    meta: {
+      title: '权限管理',
+      icon: 'Lock',
+    },
+    children: [
+      {
+        path: '/acl/user',
+        name: 'User',
+        component: () => import('@/views/acl/user/index.vue'),
+        meta: {
+          title: '用户管理',
+          icon: 'User',
+        },
+      },
+      {
+        path: '/acl/role',
+        name: 'Role',
+        component: () => import('@/views/acl/role/index.vue'),
+        meta: {
+          title: '角色管理',
+          icon: 'UserFilled',
+        },
+      },
+      {
+        path: '/acl/permission',
+        name: 'Permission',
+        component: () => import('@/views/acl/permission/index.vue'),
+        meta: {
+          title: '菜单管理',
+          icon: 'Monitor',
+        },
+      },
+    ],
+  },
+  {
+    path: '/product',
+    name: 'Product',
+    component: () => import('@/components/Layout/index.vue'),
+    meta: {
+      title: '商品管理',
+      icon: 'Goods',
+    },
+    children: [
+      {
+        path: '/product/trademark',
+        name: 'Trademark',
+        component: () => import('@/views/product/trademark/index.vue'),
+        meta: {
+          title: '品牌管理',
+          icon: 'ShoppingCartFull',
+        },
+      },
+      {
+        path: '/product/attr',
+        name: 'Attr',
+        component: () => import('@/views/product/attr/index.vue'),
+        meta: {
+          title: '属性管理',
+          icon: 'ChromeFilled',
+        },
+      },
+      {
+        path: '/product/spu',
+        name: 'Spu',
+        component: () => import('@/views/product/spu/index.vue'),
+        meta: {
+          title: 'SPU管理',
+          icon: 'Calendar',
+        },
+      },
+      {
+        path: '/product/sku',
+        name: 'Sku',
+        component: () => import('@/views/product/sku/index.vue'),
+        meta: {
+          title: 'SKU管理',
+          icon: 'Orange',
+        },
+      },
+    ],
   },
 ]

efc7c551b5b2ca2a3a74f993b33f2d8b920c12a3
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-25 18:38:54 +0800

    feat: 编写组件展示layout布局中的内容(并添加动画效果)

---
+++ src/components/Layout/Main/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <router-view v-slot="{ Component }">
+      <transition name="fade">
+        <!-- 渲染layout一级路由组件的子路由 -->
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.fade-enter-from {
+  opacity: 0;
+  transform: scale(0);
+}
+
+.fade-enter-active {
+  transition: all 1s;
+}
+.fade-enter-to {
+  opacity: 1;
+  transform: scale(1);
+}
+</style>

9e6280b02dd6fec7bb08898cfc9ed3917d003380
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-26 17:04:27 +0800

    refactor: 发送请求获取用户信息

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,6 +4,16 @@
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+//获取仓库
+import { userStore } from '@/store/modules/user'
+
+const useUserStore = userStore()
+//首页挂载完毕，发请求获取用户信息
+onMounted(() => {
+  useUserStore.getUserInfo()
+})
+</script>
 
 <style scoped land="scss"></style>

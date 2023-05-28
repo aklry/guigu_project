a6d2006f1125dac2d58f12da33fd061b4b958972
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-28 21:57:11 +0800

    feat: 搭建新增和编辑对话框布局

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -2,35 +2,19 @@
   <div>
     <el-card class="box-card">
       <!-- 卡片顶部添加品牌按钮 -->
-      <el-button type="primary" icon="Plus" @click="addTrademarkHandle">
-        添加品牌
-      </el-button>
+      <el-button type="primary" icon="Plus" @click="addTrademarkHandle">添加品牌</el-button>
       <!-- 表格数据,用于展示已有品牌数据 -->
       <el-table :data="trademarkArr" style="margin: 10px 0" border>
-        <el-table-column
-          label="序号"
-          width="80"
-          align="center"
-          type="index"
-        ></el-table-column>
+        <el-table-column label="序号" width="80" align="center" type="index"></el-table-column>
         <el-table-column label="品牌名称" prop="tmName"></el-table-column>
         <el-table-column label="品牌LOGO">
           <template #="{ row }">
-            <img
-              :src="row.logoUrl"
-              alt=""
-              style="width: 100px; height: 100px"
-            />
+            <img :src="row.logoUrl" alt="" style="width: 100px; height: 100px" />
           </template>
         </el-table-column>
         <el-table-column label="品牌操作">
           <template #="{ row, $index }">
-            <el-button
-              type="primary"
-              size="small"
-              icon="Edit"
-              @click="editTrademarkHandle"
-            />
+            <el-button type="primary" size="small" icon="Edit" @click="editTrademarkHandle" />
             <el-button size="small" type="danger" icon="Delete" />
           </template>
         </el-table-column>
@@ -40,30 +24,19 @@
       v-model:page-size="":设置每页多少条数据
       page-sizes:每页显示个数选择器的选项设置
     -->
-      <el-pagination
-        v-model:current-page="currentPage"
-        v-model:page-size="pageSize"
-        :page-sizes="pageSizes"
-        layout="prev, pager, next, jumper, ->, total, sizes, "
-        @current-change="getHasTradeMark"
-        @size-change="pageSizeChange"
-        :total="total"
-      />
+      <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="pageSizes"
+        layout="prev, pager, next, jumper, ->, total, sizes, " @current-change="getHasTradeMark"
+        @size-change="pageSizeChange" :total="total" />
     </el-card>
     <!-- 对话框组件：用于添加品牌与修改品牌 -->
     <el-dialog v-model="dialogFormVisible" title="添加品牌">
-      <el-form style="width: 80%">
+      <el-form style="width: 80%;">
         <el-form-item label="品牌名称" label-width="80">
           <el-input placeholder="请您输入品牌名称" />
         </el-form-item>
         <el-form-item label="品牌LOGO" label-width="80">
-          <el-upload
-            class="avatar-uploader"
-            action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-            :show-file-list="false"
-            :on-success="handleAvatarSuccess"
-            :before-upload="beforeAvatarUpload"
-          >
+          <el-upload class="avatar-uploader" action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+            :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
             <img v-if="imageUrl" :src="imageUrl" class="avatar" />
             <el-icon v-else class="avatar-uploader-icon">
               <Plus />
@@ -72,12 +45,8 @@
         </el-form-item>
       </el-form>
       <template #footer>
-        <el-button type="default" @click="dialogFormVisible = false">
-          取消
-        </el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">
-          确认
-        </el-button>
+        <el-button type="default" @click="dialogFormVisible = false">取消</el-button>
+        <el-button type="primary" @click="confirmSubmitHandle">确认</el-button>
       </template>
     </el-dialog>
   </div>
@@ -139,6 +108,10 @@ const editTrademarkHandle = () => {
   dialogFormVisible.value = true
 }
 //对话框底部确认按钮
+const confirmSubmitHandle = () => {
+  //对话框隐藏
+  dialogFormVisible.value = false
+}
 </script>
 
 <style>

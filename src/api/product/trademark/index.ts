28399d951fa18db4f004987cbf2df13505e10413
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-28 19:28:38 +0800

    style: 代码格式修改

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -4,15 +4,18 @@ import type { TrademarkResponseData } from './type'
 //品牌管理模块接口地址
 
 enum API {
-    //获取已有品牌接口
-    TRADEMARK_URL='/admin/product/baseTrademark/'
+  //获取已有品牌接口
+  TRADEMARK_URL = '/admin/product/baseTrademark/',
 }
 
 //获取已有品牌的方法
 /**
- * 
+ *
  * @param page 当前第几页
  * @param limit 每页多少条数据
- * @returns 
+ * @returns
  */
-export const reqHasTrademark = (page: number, limit: number) => request.get<any, TrademarkResponseData>(API.TRADEMARK_URL + `${page}/${limit}`)
+export const reqHasTrademark = (page: number, limit: number) =>
+  request.get<any, TrademarkResponseData>(
+    API.TRADEMARK_URL + `${page}/${limit}`,
+  )

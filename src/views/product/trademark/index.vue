29f1b1a1408ce3fcb4edc201b395681b3dbd3c25
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-28 19:29:21 +0800

    feat: 实现品牌管理数据的分页展示

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -3,12 +3,17 @@
     <!-- 卡片顶部添加品牌按钮 -->
     <el-button type="primary" icon="Plus">添加品牌</el-button>
     <!-- 表格数据,用于展示已有品牌数据 -->
-    <el-table :data="trademarkArr" style="margin: 10px 0;" border>
-      <el-table-column label="序号" width="80" align="center" type="index"></el-table-column>
+    <el-table :data="trademarkArr" style="margin: 10px 0" border>
+      <el-table-column
+        label="序号"
+        width="80"
+        align="center"
+        type="index"
+      ></el-table-column>
       <el-table-column label="品牌名称" prop="tmName"></el-table-column>
       <el-table-column label="品牌LOGO">
         <template #="{ row }">
-          <img :src="row.logoUrl" alt="" style="width: 100px; height: 100px;">
+          <img :src="row.logoUrl" alt="" style="width: 100px; height: 100px" />
         </template>
       </el-table-column>
       <el-table-column label="品牌操作">
@@ -23,8 +28,15 @@
       v-model:page-size="":设置每页多少条数据
       page-sizes:每页显示个数选择器的选项设置
     -->
-    <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="pageSizes"
-      layout="prev, pager, next, jumper, ->, total, sizes, " :total="total" />
+    <el-pagination
+      v-model:current-page="currentPage"
+      v-model:page-size="pageSize"
+      :page-sizes="pageSizes"
+      layout="prev, pager, next, jumper, ->, total, sizes, "
+      @current-change="getHasTradeMark"
+      @size-change="pageSizeChange"
+      :total="total"
+    />
   </el-card>
 </template>
 
@@ -32,7 +44,10 @@
 import { ref, onMounted } from 'vue'
 import { reqHasTrademark } from '@/api/product/trademark/index.ts'
 //引入数据类型
-import type { Records, TrademarkResponseData } from '@/api/product/trademark/type'
+import type {
+  Records,
+  TrademarkResponseData,
+} from '@/api/product/trademark/type'
 
 //当前页码
 let currentPage = ref<number>(1)
@@ -45,8 +60,12 @@ let total = ref<number>(0)
 //定义存储已有品牌的容器
 let trademarkArr = ref<Records>([])
 //获取已有品牌数据
-const getHasTradeMark = async () => {
-  let result: TrademarkResponseData = await reqHasTrademark(currentPage.value, pageSize.value)
+const getHasTradeMark = async (page = 1) => {
+  currentPage.value = page
+  let result: TrademarkResponseData = await reqHasTrademark(
+    currentPage.value,
+    pageSize.value,
+  )
   if (result.code === 200) {
     total.value = result.data.total
     trademarkArr.value = result.data.records
@@ -55,6 +74,17 @@ const getHasTradeMark = async () => {
 onMounted(() => {
   getHasTradeMark()
 })
+//分页器当前页码改变时
+// const changeCurrentPage = () => {
+//   getHasTradeMark()
+// }
+
+//当下拉菜单发生变化时
+const pageSizeChange = () => {
+  //当数据量发生变化时，让当前页码归一
+  // currentPage.value = 1
+  getHasTradeMark()
+}
 </script>
 
 <style scoped></style>

7e50afd2f2aa647d516d4db1706fd7d2247ca9a5
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-29 23:27:53 +0800

    refactor: 定义仓库中数据的类型

--- src/store/modules/types/type.ts
+++ src/store/modules/types/type.ts
@@ -1,8 +1,17 @@
 import type { RouteRecordRaw } from 'vue-router'
+import type { CategoryObject } from '@/api/product/attr/type'
 //定义仓库state类型
 export interface UserState {
   token: string | null
   menuRoutes: RouteRecordRaw[]
   username: string
   avatar: string
+}
+export interface CategoryState {
+  c1Data: CategoryObject[]
+  c1Id: string | number
+  c2Id: string | number
+  c3Id: string | number
+  c2Data: CategoryObject[]
+  c3Data: CategoryObject[]
 }

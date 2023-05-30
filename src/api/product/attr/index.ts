e0b5a5152137b4e76054f6435532f2d8a1984c49
Aklry <74409739+aklry@users.noreply.github.com>
2023-05-30 19:44:10 +0800

    feat: 定义展示已有属性与属性值的接口与数据类型

--- src/api/product/attr/index.ts
+++ src/api/product/attr/index.ts
@@ -1,6 +1,6 @@
 //属性管理相关API
 import request from '@/utils/request'
-import type { CategoryResponseData } from './type'
+import type { CategoryResponseData, AttrResponseData } from './type'
 enum API {
   //一级分类接口地址
   C1_URL = '/admin/product/getCategory1',
@@ -8,6 +8,8 @@ enum API {
   C2_URL = '/admin/product/getCategory2/',
   //三级分类接口地址
   C3_URL = '/admin/product/getCategory3/',
+  //获取分类下已有的属性与属性值
+  ATTR_URL = '/admin/product/attrInfoList/',
 }
 
 export const reqC1 = () => request.get<any, CategoryResponseData>(API.C1_URL)
@@ -17,3 +19,10 @@ export const reqC2 = (id: number | string) =>
 
 export const reqC3 = (id: number | string) =>
   request.get<any, CategoryResponseData>(API.C3_URL + id)
+
+export const reqAttr = (
+  c1Id: number | string,
+  c2Id: number | string,
+  c3Id: number | string,
+) =>
+  request.get<any, AttrResponseData>(API.ATTR_URL + `${c1Id}/${c2Id}/${c3Id}`)
--- src/api/product/attr/type.ts
+++ src/api/product/attr/type.ts
@@ -16,3 +16,30 @@ export interface CategoryObject {
 export interface CategoryResponseData extends ResponseData {
   data: CategoryObject[]
 }
+
+//属性与属性值的ts类型
+
+//属性值对象的ts类型
+export interface AttrValue {
+  id: number
+  valueName: string
+  attrId: number
+}
+//存储每一个属性值对象的数组类型
+export type attrValueList = AttrValue[]
+//属性对象
+export interface Attr {
+  id: number
+  attrName: string
+  categoryId: number
+  categoryLevel: number
+  attrValueList: attrValueList
+}
+
+//存储每一个属性对象的数组的ts类型
+export type AttrList = Attr[]
+
+//属性接口返回的数据类型
+export interface AttrResponseData extends ResponseData {
+  data: Attr[]
+}
